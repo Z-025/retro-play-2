965a9550fdea6f6bbe1da38316dc10cf1e78a6bb
Pamela Azúa <pam.azua@duocuc.cl>
2025-07-15 14:08:22 +0900

    test correction

--- src/app/pages/home/home.spec.ts
+++ src/app/pages/home/home.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+// 1. Importa el módulo para pruebas HTTP
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Home } from './home';
 
 describe('Home', () => {
@@ -8,10 +9,11 @@ describe('Home', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Home]
+      // 2. Añade HttpClientTestingModule a los imports
+      imports: [Home, HttpClientTestingModule]
     })
     .compileComponents();
-
+    
     fixture = TestBed.createComponent(Home);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/pages/tienda/tienda.spec.ts
+++ src/app/pages/tienda/tienda.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Tienda } from './tienda';
 
 describe('Tienda', () => {
@@ -8,10 +9,11 @@ describe('Tienda', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Tienda]
+
+      imports: [Tienda, HttpClientTestingModule]
     })
     .compileComponents();
-
+    
     fixture = TestBed.createComponent(Tienda);
     component = fixture.componentInstance;
     fixture.detectChanges();
