71cfbb230ebefc4404d738ae10a76ad29a8de416
Pamela Azúa <pam.azua@duocuc.cl>
2025-07-21 05:13:44 +0900

    EFT FINAL

--- src/app/pages/register/register.spec.ts
+++ src/app/pages/register/register.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Register } from './register';
 
 describe('Register', () => {
@@ -8,10 +9,10 @@ describe('Register', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Register]
+      imports: [ Register, ReactiveFormsModule, HttpClientTestingModule ]
     })
     .compileComponents();
-
+    
     fixture = TestBed.createComponent(Register);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +20,22 @@ describe('Register', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('el formulario debe ser inválido si la contraseña es muy corta', () => {
+    const password = component.registerForm.get('password');
+    password?.setValue('123'); // Menos de 8 caracteres
+    expect(password?.valid).toBeFalsy();
+    expect(password?.hasError('minlength')).toBe(true);
+  });
+
+  it('el formulario debe ser inválido si las contraseñas no coinciden', () => {
+    const password = component.registerForm.get('password');
+    const confirmPassword = component.registerForm.get('confirmPassword');
+
+    password?.setValue('Password123*');
+    confirmPassword?.setValue('PasswordDiferente');
+    
+    expect(component.registerForm.hasError('passwordsNotMatching')).toBe(true);
   });
 });
